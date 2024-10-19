27fc73aaa6805a8646fbfc9ed0c64cb58b363750
afquidangen <alyssafayequidangen@gmail.com>
2024-10-20 01:27:09 +0800

    Array Activity

---
+++ arrayActivity.js
@@ -0,0 +1,32 @@
+let favSingers = ["Nicole Zefanya", "Taylor Swift", "Harry Styles"];
+console.log(favSingers[0]);
+//output: Nicole Zefanya
+
+let favNumbers = ["2", "12", "14", "28"];
+
+let mixedArr = [28, 24.5, "string", ["other array"], false];
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
+console.log(mixedArr[4]);
+
+//output:
+// 28
+// 24.5
+// string
+// [ 'other array' ]
+// false
+
+console.log(typeof mixedArr[0]);
+console.log(typeof mixedArr[1]);
+console.log(typeof mixedArr[2]);
+console.log(typeof mixedArr[3]);
+console.log(typeof mixedArr[4]);
+//output:
+// number
+// number
+// string
+// object
+// boolean
+
